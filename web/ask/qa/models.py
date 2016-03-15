3b7b281ebd69625e4a4abe1a891cebeae6fe8aae
miholeus <me@miholeus.com>
2016-03-16 00:12:47 +0300

    first model

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User as BaseUser
 
-# Create your models here.
+class User(BaseUser):
+    class Meta:
+        db_table = 'users'
+
+class Question(models.Model):
+    title=models.CharField(max_length=255)
+    text=models.TextField()
+    added_at=models.DateTimeField(auto_now=True, db_index=True)
+    rating=models.IntegerField(default=0)
+    author=models.ForeignKey(User, related_name='question_author_id')
+
+    likes=models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return '/question/%d/' % self.pk
+    class Meta:
+        db_table = 'questions'
+        ordering = ['-added_at']
+
+class Answer(models.Model):
+    text=models.TextField()
+    added_at=models.DateTimeField(auto_now=True, db_index=True)
+    question=models.OneToOneField(Question)
+    author=models.ForeignKey(User)
+
+    def __str__(self):
+        return self.text
+    class Meta:
+        db_table = 'answers'
+        ordering = ['-added_at']
--- web/ask/qa/views.py
+++ web/ask/qa/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+    return render(request, 'b.html')
 
 def not_found(request, *args, **kwargs):
     return HttpResponseNotFound("<h1>Page not Found</h1>")
